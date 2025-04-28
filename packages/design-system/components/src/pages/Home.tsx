a7a19f226009d77b4e113a5eb554eb5361e8a03e
eo9on9 <eo9on9@gmail.com>
2025-04-28 10:23:59 +0900

    notes: Fonts

--- packages/design-system/components/src/pages/Home.tsx
+++ packages/design-system/components/src/pages/Home.tsx
@@ -1,3 +1,14 @@
+const buttonClassMap = {
+  primary: 'bg-forest text-white',
+  secondary: 'bg-white text-forest border-forest border',
+
+  sm: 'h-8 px-4',
+  md: 'h-10 px-6',
+  lg: 'h-12 px-8',
+
+  disabled: 'opacity-50 cursor-not-allowed',
+}
+
 export const Home = () => {
-  return <div className="text-forest border-forest border h-4 w-full">adsf</div>
+  return <div className={`text-forest border-forest border h-4 w-full`}>adsf</div>
 }
