48e677d0eacf885375a3a999b3f70589fcd37c21
eo9on9 <eo9on9@gmail.com>
2025-04-29 13:42:15 +0900

    chore: Set vanilla extract

--- packages/design-system/components/src/App.tsx
+++ packages/design-system/components/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
+import { Home2 } from './pages/Home2'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home2" element={<Home2 />} />
       </Routes>
     </BrowserRouter>
   )
